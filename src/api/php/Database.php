bbb0228199817215a0401cf5a72dbe8fe304a909
IsaqueDiniz <isaquediniz3713@gmail.com>
2019-01-25 20:12:29 -0200

    INSERT, UPDATE, DELETE ok

--- src/api/php/Database.php
+++ src/api/php/Database.php
@@ -10,19 +10,42 @@ class Database {
 	function __construct() {
 		try {
 			$this->PDO = new PDO($this->dsn, $this->user, $this->password);
-			// $this->query = $this->PDO->query;
+			$this->PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 		}catch (PDOExcepetion $error) {
 			$this->PDO = $error->getMessage();
 			die();
 		}
 	}
 
-	public function insertRegistry($registry) {
+	public function insertRegistry($jsonRegistry) {
 		//INSERT one registry 
+		$registry = json_decode($jsonRegistry);
+		foreach ($registry as $key => $value) {
+			$$key = $value;				
+		}
+
+		$sql = 'INSERT INTO Livro (id, livro, publicacao, autor, editora, ISBN)
+						VALUES (:id, :livro, :publicacao, :autor, :editora, :ISBN)';
+
+		$statement = $this->PDO->prepare($sql);
+		$statement->execute([
+			'id' => $id,
+			'livro' => $livro,
+			'publicacao' => $publicacao,	
+			'autor' => $autor,
+			'editora' => $editora,
+			'ISBN' => $ISBN
+		]); 	
 	}
 
 	public function deleteRegisty($registryId) {
 		//DELETE one registry 
+		$id = json_decode($registryId)->id;
+
+		$sql = 'DELETE FROM Livro WHERE id = :id';
+
+		$statement = $this->PDO->prepare($sql);
+		$statement->execute(['id' => $id]);
 	}
 
 	public function updateRegistry($registry) {
@@ -30,13 +53,23 @@ class Database {
 	}
 
 	public function selectAllRegistry() {
-		//SELECT one registry
+		//SELECT all 
 		$sql = 'SELECT id, livro, publicacao, autor, editora, ISBN FROM Livro';
 		$dataResult = $this->PDO->query($sql);
-
-		$data = json_encode($dataResult->fetchAll(PDO::FETCH_ASSOC));
-
+		$data = json_encode($dataResult->fetchAll());
 		return $data;	
 	}
 
 }
+
+// '{"id":"1234567","livro":"Nome do livro","publicacao":"2019-01-25","autor":"Nome do Autor","editora":"Editora","ISBN":"1234567"}'
+
+$db = new Database();
+
+// $db->insertRegistry('{"id":"4157897","livro":"OutroLivro","publicacao":"2019-01-25","autor":"Nome do Autor","editora":"Editora","ISBN":"1234567"}');
+
+// $result = $db->selectAllRegistry();
+
+$db->deleteRegisty('{"id" : 4157897}');
+
+	// print_r(json_decode($result));
